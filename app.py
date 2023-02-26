1bc129d211a98e4f90f32659c8e2ce0a08ce036e
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2023-02-26 12:00:11 -0600

    Update app.py
    Can't  make a db

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def add_pet():
     return render_template('addpet.html')
 
 if __name__ == '__main__':
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=8000, host='0.0.0.0')
